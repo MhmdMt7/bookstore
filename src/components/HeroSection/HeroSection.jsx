060673d5f8e098b2bdc6aa5fb2ef8c8866fa3d3b
MhmdMt7 <tolbam902@gmail.com>
2026-01-28 01:41:31 +0200

    mo_tolba

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,15 @@
+import img from "../../assets/images/mo.png";
+import Navbar from "../Navbar/Navbar";
+export default function Herosection() {
+  return (
+    <>
+      <Navbar />
+      <div
+        className="relative h-full bg-cover bg-center -z-10"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        <div className="absolute inset-0 bg-black/60"></div>
+      </div>
+    </>
+  );
+}
